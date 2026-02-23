139e2aa9cd715ee84f7d3c13cdcc9d77b3320c05
Yogesh Meena <developeryogeshh@gmail.com>
2026-02-23 22:24:19 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
-  // 👇 BASE MUST BE HERE (top-level)
- base: "/personal-portfolio/",
-
   server: {
     host: true,
   },
