bdef600fd0c8aa5ea7912ef4ba555e9b9019dd35
Yogesh Meena <developeryogeshh@gmail.com>
2026-02-27 16:59:04 +0530

    commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import nextVitals from 'eslint-config-next/core-web-vitals'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  ...nextVitals,
+  globalIgnores(['.next/**', 'out/**', 'build/**', 'dist/**']),
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
+    files: ['**/*.{js,jsx,mjs,cjs}'],
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
     },
